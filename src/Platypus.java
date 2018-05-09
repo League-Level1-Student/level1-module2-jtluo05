7649dc2d6cbf9734b5d6dd002bfe7ee264125d42
jtluo05 <jtluo05@gmail.com>
2018-05-08 19:57:29 -0700

    platypus

--- src/Platypus.java
+++ src/Platypus.java
@@ -1,10 +1,18 @@
 
 public class Platypus {
       private String name;
-
+ Platypus(String name){
+     this.name=name;
+      }
+ public void setName(String n) {
+	 name=n;
+ }
       public void sayHi(){
-            	System.out.println("The platypus " + name + 
+     
+      
+    	  System.out.println("The platypus " + name + 
             			" is smarter than your average platypus.");
       	}
+     
 }
 
--- src/PlatypusRunner.java
+++ src/PlatypusRunner.java
@@ -2,8 +2,9 @@
 public class PlatypusRunner {
 public static void main(String[] args) {
 	
-	Platypus platypus=new Platypus();
+	Platypus platypus=new Platypus("Perry");
   platypus.sayHi();
+  platypus.setName("Perry");
 }
 
 }
