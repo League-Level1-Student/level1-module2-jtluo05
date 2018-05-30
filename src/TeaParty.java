72d74f9bed48cb82626bcc1de41664fa957f231b
League <league@WTS13.attlocal.net>
2018-05-29 19:59:24 -0700

    tea party, checkpoint 2 constructers level 1

--- src/TeaParty.java
+++ src/TeaParty.java
@@ -1,14 +1,40 @@
+import javax.imageio.ImageTypeSpecifier;
 
 public class TeaParty {
+	String name;
+	boolean isWoman;
+	boolean isKnighted;
 
-	 public String welcome(String name, boolean isWoman, boolean isKnighted)
-     {
+	public String welcome(String name, boolean isWoman, boolean isKnighted) {
+		String message = "Hello";
+		this.name = name;
+		this.isWoman = isWoman;
+		this.isKnighted = isKnighted;
+		if (isKnighted & !isWoman) {
+		message=	 message +" Sir " + name;
+		}
+		else if (isKnighted & isWoman) {
+			message= message +" Lady " + name;
+		} else if (isWoman) {
+		message=	 message +" Ms. " + name;
+		}else if (!isWoman){
+		message=	 message +" Mr. " + name;
+		}
+		
+		System.out.println(message);
+return message;
+	}
 
-return name;
-     }
+	String getName() {
+		return name;
+	}
 
+	boolean getGender() {
+		return isWoman;
+	}
 
+	boolean getEyes() {
+		return isKnighted;
+	}
 
-
-	
 }
